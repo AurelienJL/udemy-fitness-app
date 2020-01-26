a7f6a1a6693fe994ae2aa6be5d69b769c6358a35
AurelienJL <aurelienjl@live.fr>
2020-01-26 19:50:28 +0100

    angularfire part 2

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Exercise } from '../exercise.model';
-import { TrainingService } from '../training.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { TrainingService } from '../training.service';
+import { Exercise } from '../exercise.model';
 
 
 @Component({
@@ -12,28 +10,28 @@ import { map } from 'rxjs/operators';
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.scss']
 })
-export class NewTrainingComponent implements OnInit {
-  availableExercises: Observable<any>;
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  availableExercises: Exercise[];
+  exercisesSubscription: Subscription;
 
   constructor(
-    private trainingService: TrainingService,
-    private angularFirestore: AngularFirestore) { }
+    private trainingService: TrainingService
+    ) { }
 
   ngOnInit() {
-   this.availableExercises =
-   this.angularFirestore.collection('availableExercises')
-   .snapshotChanges()
-   .pipe(map(docArray => {
-     return docArray.map(doc => {
-       return {
-         id: doc.payload.doc.id,
-         ...doc.payload.doc.data()
-       } as Exercise;
-     });
-   }));
+    this.exercisesSubscription = this.trainingService.exercisesChanged.subscribe(
+      (exercises: Exercise[]) => {
+        this.availableExercises = exercises;
+      }
+    );
+    this.trainingService.fecthAvailableExercises();
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
+  }
+
+  ngOnDestroy() {
+    this.exercisesSubscription.unsubscribe();
   }
 }
